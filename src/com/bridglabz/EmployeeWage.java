c8af2acc27645f0de2b7c7ab68165b6c1638fa72
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-21 07:44:11 +0530

    Added; Check whether employee is present or Absent & calculate his wage according to hours using Switch Case

--- src/com/bridglabz/EmployeeWage.java
+++ src/com/bridglabz/EmployeeWage.java
@@ -1,23 +1,29 @@
 package com.bridglabz;
 
 public class EmployeeWage {
-        static final int Wage_Per_Hour=20;
-        static final int Full_Time_Hour=8;
-        static final int Part_Time_Hour=4;
-        public static void main(String[] args) {
-            int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
-            System.out.println(employeeAttendance);
-            int dailyWage=0;
-            if (employeeAttendance==1){
+    static final int Wage_Per_Hour = 20;
+    static final int Full_Time_Hour = 8;
+    static final int Part_Time_Hour = 4;
+    static final int Is_Part_Time_Hour=1;
+    static final int Is_Full_Time_Hour=2;
+
+    public static void main(String[] args) {
+        int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println(employeeAttendance);
+        int dailyWage = 0;
+        switch (employeeAttendance) {
+            case Is_Full_Time_Hour:
                 System.out.println("Employee is Present");
-                dailyWage=Wage_Per_Hour*Full_Time_Hour;
-            }
-            else if (employeeAttendance==2) {
+                dailyWage = Wage_Per_Hour * Full_Time_Hour;
+                System.out.println("Employee Daily Wage Is " + dailyWage);
+                break;
+            case Is_Part_Time_Hour:
                 dailyWage = Wage_Per_Hour * Part_Time_Hour;
-            }
-            else {
-                System.out.println("Employee is absent");
-            }
-            System.out.println("daily Wage==>"+dailyWage);
+                System.out.println("Employee is Part Time");
+                System.out.println("Employee Daily Wage Is " + dailyWage);
+                break;
+            default:
+                System.out.println("Absent");
         }
+    }
 }
