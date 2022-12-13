7ad4fb77cf0c15ee5b2a2026dc197b6f34d67599
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-13 07:46:04 +0530

    Added:Refacter code using Interface approch

---
+++ src/com/bridglabz/Interface.java
@@ -0,0 +1,9 @@
+package com.bridglabz;
+
+public interface Interface {
+    int calculateWage(int Wage_Per_Hour, int Working_Days_Per_Month, int Working_Hours_Per_Month);
+     int Full_Time_Hour = 8;
+     int Part_Time_Hour = 4;
+     int Is_Part_Time_Hour = 1;
+     int Is_Full_Time_Hour = 2;
+}
